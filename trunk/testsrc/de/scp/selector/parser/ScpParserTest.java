dc430db296fa3ebb9781aff32769ababf9ff8f36
crypter <crypter@509c41a6-7901-0410-9f1b-c61645519166>
2007-04-12 13:00:50 +0000

    Extended parser.

--- trunk/testsrc/de/scp/selector/parser/ScpParserTest.java
+++ trunk/testsrc/de/scp/selector/parser/ScpParserTest.java
@@ -3,11 +3,11 @@ package de.scp.selector.parser;
 import junit.framework.TestCase;
 import de.scp.selector.parser.generated.ScpParser;
 import de.scp.selector.ruleengine.Knowledgebase;
+import de.scp.selector.ruleengine.Session;
 
 public class ScpParserTest extends TestCase {
 
 	public void testBooleanLogic() {
-		ScpParser.init();
 		String[][] testCases = { { "true and false", "false" }, { "true and true", "true" },
 				{ "false and false", "false" }, { "false and true", "false" },
 
@@ -25,22 +25,29 @@ public class ScpParserTest extends TestCase {
 				{ "(true and not false) or (not true and false)", "true" }
 
 		};
-//		for (int i = 0; i < testCases.length; i++) {
-//			assertTrue(testCases[i][0] + " expected " + testCases[i][1] + ", but failed", Boolean
-//					.parseBoolean(testCases[i][1]) == ScpParser.evaluate(testCases[i][0]));
-//		}
+		// for (int i = 0; i < testCases.length; i++) {
+		// assertTrue(testCases[i][0] + " expected " + testCases[i][1] + ", but failed", Boolean
+		// .parseBoolean(testCases[i][1]) == ScpParser.evaluate(testCases[i][0]));
+		// }
 	}
 
 	public void testRules() {
-		String[] testCases = { 
-				"knowledgebase Armin_01; \n" +
-				"  define enum a = {\"aa\",\"bb\"}; \n" +
-				"  define enum b = {\"aa\",\"bb\"}; \n" +
-				"  if (not a=\"a\") then b = \"aa\";\n" +
-				"end\n" };
+		String[][] testCases = { new String[] {
+				"knowledgebase Armin_01; \n" 
+						+ "  define enum a = {\"aa\",\"bb\"}; \n"
+						+ "  define enum b = {\"aa\",\"bb\"}; \n"
+						+ "  if (a=\"aa\") then b = \"bb\";\n" 
+						+ "end\n", 
+						"a", "aa", "b", "bb" } 
+		};
 		for (int i = 0; i < testCases.length; i++) {
-			System.out.println(testCases[i]);
-			Knowledgebase kb = ScpParser.compileKnowledgebase(testCases[i]);
+			System.out.println(testCases[i][0]);
+			Knowledgebase kb = ScpParser.compileKnowledgebase(testCases[i][0]);
+			Session session = new Session(kb);
+			session.setAttribute(testCases[i][1], testCases[i][2]);
+			assertEquals(testCases[i][0] + testCases[i][1] + " = " + testCases[i][2] +
+					" => "+testCases[i][3],
+					testCases[i][4], session.getSelectedValues(testCases[i][3])[0]);
 		}
 	}
 }
